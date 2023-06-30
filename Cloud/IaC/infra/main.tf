fc7100f87ab7b7d739b582908e4adf3ed9ba9df7
kayckdelfino <kayck.delfino@gmail.com>
2023-06-30 16:42:24 -0300

    Adding loadBalancer (IaC Cloud)

--- Cloud/IaC/infra/main.tf
+++ Cloud/IaC/infra/main.tf
@@ -30,12 +30,47 @@ resource "aws_key_pair" "SSHkey" {
 }
 
 resource "aws_autoscaling_group" "group" {
-  availability_zones = ["${var.aws_region}a"]
+  availability_zones = ["${var.aws_region}a", "${var.aws_region}b"]
   name               = var.groupName
   max_size           = var.max
   min_size           = var.min
   launch_template {
     id      = aws_launch_template.maquina.id
     version = "$Latest"
+  }
+  target_group_arns    = [aws_lb_target_group.loadBalancerTarget.arn]
+}
+
+resource "aws_default_subnet" "subnet_1" {
+  availability_zone = "${var.aws_region}a"
+}
+
+resource "aws_default_subnet" "subnet_2" {
+  availability_zone = "${var.aws_region}b"
+}
+
+resource "aws_lb" "loadBalancer" {
+  internal = false
+  subnets = [aws_default_subnet.subnet_1.id, aws_default_subnet.subnet_2.id]
+}
+
+resource "aws_default_vpc" "default" {
+  
+}
+
+resource "aws_lb_target_group" "loadBalancerTarget" {
+  name = "maquinasAlvo"
+  port = "8000"
+  protocol = "HTTP"
+  vpc_id = aws_default_vpc.default.id
+}
+
+resource "aws_lb_listener" "loadBalancerInput" {
+  load_balancer_arn = aws_lb.loadBalancer.arn
+  port              = "8000"
+  protocol = "HTTP"
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.loadBalancerTarget.arn
   }
 }
