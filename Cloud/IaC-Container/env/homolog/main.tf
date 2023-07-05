b92739d8dd60d29ec44efd45768bd34baa6f6328
kayckdelfino <kayck.delfino@gmail.com>
2023-07-05 19:08:11 -0300

    Cleaning folder

--- Cloud/IaC-Container/env/homolog/main.tf
+++
@@ -1,9 +0,0 @@
-module "homolog" {
-  source = "../../infra"
-
-  appName     = "homolog"
-  desc        = "homolog-application"
-  max         = 3
-  mType       = "t2.micro"
-  environment = "homolog-environment"
-}
--- Cloud/IaC-Container/env/prod/backend.tf
+++
@@ -1,7 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket = "terraform-state-kayckdelfino"
-    key    = "prod/terraform.tfstate"
-    region = "us-east-1"
-  }
-}
