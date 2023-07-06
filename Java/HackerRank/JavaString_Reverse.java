d477985233eed02af6350a14b6a5b4c432eb6443
kayckdelfino <kayck.delfino@gmail.com>
2023-07-05 23:03:40 -0300

    Adding 16th Java exercise

---
+++ Java/HackerRank/JavaString_Reverse.java
@@ -0,0 +1,34 @@
+/*
+A palindrome is a word, phrase, number, or other sequence of characters which reads the same backward or forward.
+
+Given a string A, print Yes if it is a palindrome, print No otherwise.
+
+Constraints
+- A will consist at most 50 lower case english letters.
+
+Sample Input
+madam
+
+Sample Output
+Yes
+*/
+
+package KayckGitHub.public_knowledge_base.Java.HackerRank;
+
+import java.util.Scanner;
+
+public class JavaString_Reverse {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+        sc.close();
+
+        String reversed = new StringBuilder(s).reverse().toString();
+
+        if (s.equals(reversed)) {
+            System.out.println("Yes");
+        } else {
+            System.out.println("No");
+        }
+    }
+}
