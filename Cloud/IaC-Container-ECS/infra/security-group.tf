3630676fd431920ce281cda72b7b52fe28ed1ff0
kayckdelfino <kayck.delfino@gmail.com>
2023-07-05 23:49:58 -0300

    Adding SG config (IaC ECS)

---
+++ Cloud/IaC-Container-ECS/infra/security-group.tf
@@ -0,0 +1,45 @@
+resource "aws_security_group" "alb" {
+  name        = "alb-ECS"
+  vpc_id      = module.vpc.vpc_id
+}
+
+resource "aws_security_group_rule" "tcp_alb" {
+  type              = "ingress"
+  from_port         = 8000
+  to_port           = 8000
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.alb.id
+}
+
+resource "aws_security_group_rule" "tcp_alb" {
+  type              = "egress"
+  from_port         = 0
+  to_port           = 0
+  protocol          = "-1"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.alb.id
+}
+
+resource "aws_security_group" "private" {
+  name        = "private-ECS"
+  vpc_id      = module.vpc.vpc_id
+}
+
+resource "aws_security_group_rule" "private_ECS" {
+  type              = "ingress"
+  from_port         = 0
+  to_port           = 0
+  protocol          = "-1"
+  source_security_group_id = aws_security_group.alb.id
+  security_group_id = aws_security_group.private.id
+}
+
+resource "aws_security_group_rule" "private_ECS" {
+  type              = "egress"
+  from_port         = 0
+  to_port           = 0
+  protocol          = "-1"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.private.id
+}
