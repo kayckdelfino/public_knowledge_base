bd152294dcd38c3919a3618bdcd3803e2ae44bd7
kayckdelfino <kayck.delfino@gmail.com>
2023-08-06 17:37:27 -0300

    Correcting exercise (28th CS50P)

--- Python/HarvardX CS50P/Unit Tests/test_plates/test_plates.py
+++ Python/HarvardX CS50P/Unit Tests/test_plates/test_plates.py
@@ -43,5 +43,6 @@ def test():
     assert is_valid("CS05") == False
     assert is_valid("CS50P") == False
     assert is_valid("PI3.14") == False
+    assert is_valid("50") == False
     assert is_valid("H") == False
     assert is_valid("OUTATIME") == False
