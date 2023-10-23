b535f2ec38c9b6bc311b8f71c91b678d83ef2914
kayckdelfino <kayck.delfino@gmail.com>
2023-10-23 14:42:04 -0300

    Adding 1st SQL exercise

---
+++ SQL/HackerRank/Revising_the_Select_Query_I.sql
@@ -0,0 +1,10 @@
+-- Query all columns for all American cities in the CITY table with populations larger than 100000. The CountryCode for America is USA.
+-- 
+-- The CITY table is described as follows:
+-- 
+-- <image>(https://s3.amazonaws.com/hr-challenge-images/8137/1449729804-f21d187d0f-CITY.jpg)
+
+
+SELECT *
+FROM CITY
+WHERE population > 100000 AND countrycode = "USA";
