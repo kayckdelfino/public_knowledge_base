457a6c881ae2f81fa6f31cb61506e0c575a2573c
kayckdelfino <kayck.delfino@gmail.com>
2023-10-25 22:55:10 -0300

    Adding 3rd SQL exercise

---
+++ SQL/HackerRank/Select_All.sql
@@ -0,0 +1,9 @@
+-- Query all columns (attributes) for every row in the CITY table.
+-- 
+-- The CITY table is described as follows:
+-- 
+-- <image>(https://s3.amazonaws.com/hr-challenge-images/8137/1449729804-f21d187d0f-CITY.jpg)
+
+
+SELECT *
+FROM city;
