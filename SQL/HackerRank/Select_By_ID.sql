1929372118a6bc36b19bfcae99343255b790331c
kayckdelfino <kayck.delfino@gmail.com>
2023-10-26 21:13:39 -0300

    Adding 4th SQL exercise

---
+++ SQL/HackerRank/Select_By_ID.sql
@@ -0,0 +1,10 @@
+-- Query all columns for a city in CITY with the ID 1661.
+-- 
+-- The CITY table is described as follows:
+-- 
+-- <image>(https://s3.amazonaws.com/hr-challenge-images/8137/1449729804-f21d187d0f-CITY.jpg)
+
+
+SELECT *
+FROM city
+WHERE id = 1661;
