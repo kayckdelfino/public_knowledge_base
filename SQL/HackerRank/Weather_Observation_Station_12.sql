4a2b3b173c6b4cbaac92da098eb0915c83aeb1f3
kayckdelfino <kayck.delfino@gmail.com>
2023-11-06 22:36:26 -0300

    Adding 17th SQL exercise

---
+++ SQL/HackerRank/Weather_Observation_Station_12.sql
@@ -0,0 +1,13 @@
+-- Query the list of CITY names from STATION that do not start with vowels and do not end with vowels. Your result cannot contain duplicates.
+-- 
+-- Input Format
+-- The STATION table is described as follows:
+-- 
+-- <image>(https://www.hackerrank.com/challenges/weather-observation-station-12/problem?isFullScreen=true)
+-- 
+-- where LAT_N is the northern latitude and LONG_W is the western longitude.
+
+
+SELECT DISTINCT city
+FROM station
+WHERE LEFT(city, 1) not in ("A", "E", "I", "O", "U") and RIGHT(city, 1) not in ("A", "E", "I", "O", "U");
