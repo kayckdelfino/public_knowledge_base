d91aa8d5e08e964f2fcadfb6a94237060e7afc6b
kayckdelfino <kayck.delfino@gmail.com>
2024-02-29 19:49:47 -0300

    Adding 1st Linux Shell exercise

---
+++ Shell/HackerRank/Solutions/Bash/Let's Echo/lets_echo.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Challenge statement: https://www.hackerrank.com/challenges/bash-tutorials-lets-echo/problem
+# 
+# Write a bash script that prints the string "HELLO".
+# 
+# Input Format
+# There is no input file required for this problem.
+# 
+# Output Format
+# HELLO
+# 
+# Sample Input
+# -
+# 
+# Sample Output
+# HELLO
+# 
+# Explanation
+# -
+
+
+echo "HELLO"
