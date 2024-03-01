91aa9f77f94a0f6f53a1e076657529b8ffed9960
kayckdelfino <kayck.delfino@gmail.com>
2024-03-01 17:33:12 -0300

    Adding 3rd Linux Shell exercise

---
+++ Shell/HackerRank/Solutions/Bash/Looping with Numbers/looping_with_numbers.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Challenge statement: https://www.hackerrank.com/challenges/bash-tutorials---looping-with-numbers/problem
+# 
+# Use a for loop to display the natural numbers from 1 to 50.
+# 
+# Input Format
+# There is no input
+# 
+# Output Format
+# 1
+# 2
+# 3
+# 4
+# 5
+# .
+# .
+# .
+# .
+# .
+# 50
+
+
+for n in {1..50} 
+do 
+    echo $n 
+done
