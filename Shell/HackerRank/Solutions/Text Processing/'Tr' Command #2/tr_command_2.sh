7870273f1006c9e9fc6a59be9156459ea9d7bee8
kayckdelfino <kayck.delfino@gmail.com>
2024-05-07 18:08:09 -0300

    Adding 23rd Linux Shell exercise

---
+++ Shell/HackerRank/Solutions/Text Processing/'Tr' Command #2/tr_command_2.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Challenge statement: https://www.hackerrank.com/challenges/text-processing-tr-2/problem
+# 
+# In this challenge, we practice using the tr command because it is a useful translation tool in Linux.
+# 
+# In a given fragment of text, delete all the lowercase characters a - z.
+# 
+# Input Format
+# A block of ASCII text.
+# 
+# Output Format
+# Delete all the lowercase characters in the given block of text.
+# 
+# Sample Input
+# Hello
+# World
+# how are you
+# 
+# Sample Output
+# H
+# W
+
+
+tr -d "[a-z]"
