ec49185436c3298b8237f86c5f7eee0c9cae7009
kayckdelfino <kayck.delfino@gmail.com>
2024-05-08 14:39:21 -0300

    Adding 24th Linux Shell exercise

---
+++ Shell/HackerRank/Solutions/Text Processing/'Tr' Command #3/tr_command_3.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Challenge statement: https://www.hackerrank.com/challenges/text-processing-tr-3/problem
+# 
+# In a given fragment of text, replace all sequences of multiple spaces with just one space.
+# 
+# Input Format
+# A block of ASCII text.
+# 
+# Output Format
+# Replace all sequences of multiple spaces with just one space.
+# 
+# Sample Input
+# He  llo
+# Wor  ld
+# how  are  you
+# 
+# Sample Output
+# He llo
+# Wor ld
+# how are you
+
+
+tr -s " "
